36d8e8452d6fe856a54e88a2771ec04e4b29bbc3
WebWithAstha <lodhiastha85@gmail.com>
2024-05-26 13:14:47 +0530

    asyncErrors handled

--- controllers/indexRouterController.js
+++ controllers/indexRouterController.js
@@ -1,3 +1,18 @@
-exports.homePage = function(req,res){
+const { catchAsyncErrors } = require("../middlewares/catchAsyncErrors");
+
+
+// async error handled using trycatch
+
+/*exports.homePage = async function(req,res){
+    try {
+        res.status(200).json({message:"homepage"})
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}*/
+
+
+exports.homePage = catchAsyncErrors(async function(req,res){
     res.status(200).json({message:"homepage"})
-}
+})
