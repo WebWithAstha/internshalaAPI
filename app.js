377c38d701894f7d28e1ffd93924251eb47be6f3
WebWithAstha <lodhiastha85@gmail.com>
2024-05-26 13:01:17 +0530

    error handling done

--- app.js
+++ app.js
@@ -12,5 +12,13 @@ app.use(logger("tiny"))
 // route
 app.use('/',require('./routes/indexRouters.js'))
 
+// error handler
+const ErrorHandler = require('./utils/ErrorHandler.js')
+const { generatedError } = require('./middlewares/error.js')
+app.all('*',(req,res,next)=>{
+    next(new ErrorHandler(`Requested URL not found: ${req.url}`,404))
+})
+app.use(generatedError)
+
 
 app.listen(process.env.PORT,console.log(`server running on port ${process.env.PORT}`))
