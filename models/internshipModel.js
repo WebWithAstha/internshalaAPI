612a154270de151999c0f3263e8f66d22d2c13dc
WebWithAstha <lodhiastha85@gmail.com>
2024-08-12 22:26:19 +0530

    new fields added

--- models/internshipModel.js
+++ models/internshipModel.js
@@ -3,21 +3,24 @@ const mongoose = require('mongoose')
 
 const internshipModel = new mongoose.Schema(
     {
-        profile:String,
-        employee:{type:mongoose.Schema.Types.ObjectId,ref:"employee"},
-        appliers:[{type:mongoose.Schema.Types.ObjectId,ref:"student"}],
-        skills:String,
-        internshipType:{type:String,enum:['In Office','Remote']},
-        openings:Number,
-        from:String,
-        to:String,
-        duration:String,
-        responsibility:String,
-        stipend:{
-            status:{
-                type:String,enum:['Fixed','Negotiable','Performance Based','Unpaid']},
+        profile: String,
+        employee: { type: mongoose.Schema.Types.ObjectId, ref: "employee" },
+        appliers: [{ type: mongoose.Schema.Types.ObjectId, ref: "student" }],
+        skills: [],
+        internshipType: { type: String, enum: ['In Office', 'Remote'] },
+        openings: Number,
+        from: String,
+        to: String,
+        duration: String,
+        responsibility: String,
+        stipend: {
+            status: {
+                type: String, enum: ['Fixed', 'Negotiable', 'Performance Based', 'Unpaid']
             },
-            amount:Number,
+            amount: Number,
+        },
+        perks:String,
+        assessments:String
     }
     , { timestamps: true }
 )
